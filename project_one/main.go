3809d51ccbf93d2f3d38fd2b6f853fb8630a315f
MkLateef <101887411+MkLateef@users.noreply.github.com>
2022-08-12 02:39:15 -0700

    adding stack

--- project_one/main.go
+++ project_one/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 func main(){
 	
-	fmt.Println("muhammadmubarakmk@gmail.com")
-	fmt.Println("Abdullateef Mubarak")
+	fmt.Println("Email: muhammadmubarakmk@gmail.com")
+	fmt.Println("Name: Abdullateef Mubarak")
+	fmt.Println("Stack: Backend developer")
 }
