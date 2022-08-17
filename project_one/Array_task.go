ad78dcb05bc3af6b831ac8d62ee40816f7f8a631
MkLateef <101887411+MkLateef@users.noreply.github.com>
2022-08-17 02:47:46 -0700

    Task two

---
+++ project_one/Array_task.go
@@ -0,0 +1,12 @@
+package main
+import (
+  "fmt"
+)
+
+func main(){
+Array := []int {1 , 2, 3, 4}
+ArrayString := []string{"Mubarak","Abdullateef", "Backend Dev"}
+fmt.Println("Element is: ", Array[2])
+fmt.Println("My Stack is: ", ArrayString[2] )
+
+}
