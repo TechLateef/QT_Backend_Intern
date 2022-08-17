17562131019ed651a0aa232d94c03fd6b8d1a40d
MkLateef <101887411+MkLateef@users.noreply.github.com>
2022-08-17 03:03:03 -0700

    Correction

--- project_one/Array_task.go
+++ project_one/Array_task.go
@@ -4,8 +4,8 @@ import (
 )
 
 func main(){
-Array := []int {1 , 2, 3, 4}
-ArrayString := []string{"Mubarak","Abdullateef", "Backend Dev"}
+ var array = []int {1 , 2, 3, 4}
+ var arrayString = []string{"Mubarak","Abdullateef", "Backend Dev"}
 fmt.Println("Element is: ", Array[2])
 fmt.Println("My Stack is: ", ArrayString[2] )
 
